c10523b93a508d94bbcf9fca58fdf858744a005e
Laura Vázquez Chávez <isc.lavc@gmail.com>
2025-07-22 22:32:02 -0600

    Creé la App Movies

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import RegisterView
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
